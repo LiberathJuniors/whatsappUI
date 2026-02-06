9c3b53551620d6abc74cea1eea1756afa11d7a72
LiberathJuniors <liberathjunior37@gmail.com>
2026-02-06 16:43:10 +0300

    Versioning some of the files

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/colors.dart';
+import 'package:whatsapp/screens/mobile_layout_screen.dart';
+import 'package:whatsapp/screens/web_layout_screen.dart';
+import 'package:whatsapp/utils/responsive_layout.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetFlutterBinding.enensureInitialized();
+await Firebase.initializeApp(
+  options:DefaulFirebaseOptions.currentPlatform);
+
+runApp(const MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+    );
+  }
+}
